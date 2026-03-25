a77848fa94be214cdc123f99d3b346a8c6de1c23
Svitlana Potapova <svetas.potapova@gmail.com>
2026-03-25 21:49:32 +0200

    added admin routes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,21 @@ const ProtectedRoute = ({ children }) => {
     return children;
 };
 
+const AdminRoute = ({ children }) => {
+    const isLogged = Auth.isLoggedIn();
+    const role = Auth.getRole();
+
+    if (!isLogged) {
+        return <Navigate to="/login" replace />;
+    }
+
+    if (role !== 'admin') {
+        return <Navigate to="/trips" replace />;
+    }
+
+    return children;
+};
+
 function App() {
     return (
         <Router>
@@ -22,10 +37,10 @@ function App() {
                 <Route path="/register" element={<RegisterPage />} />
                 
                 <Route path="/trips" element={<ProtectedRoute><TripsPage /></ProtectedRoute>} />
-                <Route path="/users" element={<ProtectedRoute><UsersPage /></ProtectedRoute>} />
 
-                <Route path="/users/new" element={<ProtectedRoute><CreateUserPage /></ProtectedRoute>} />
-                <Route path="/users/:id/edit" element={<ProtectedRoute><EditUserPage /></ProtectedRoute>} />
+                <Route path="/users" element={<AdminRoute><UsersPage /></AdminRoute>} />
+                <Route path="/users/new" element={<AdminRoute><CreateUserPage /></AdminRoute>} />
+                <Route path="/users/:id/edit" element={<AdminRoute><EditUserPage /></AdminRoute>} />
 
                 <Route path="/" element={<Navigate to="/trips" />} />
             </Routes>
